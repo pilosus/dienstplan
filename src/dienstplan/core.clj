4cafce1a8d0a336987af558426238a8f3aacd08a
Vitaly Samigullin <vrs@pilosus.org>
2021-12-30 00:21:18 +0300

    Run mode support

--- src/dienstplan/core.clj
+++ src/dienstplan/core.clj
@@ -3,11 +3,14 @@
   (:require
    [bidi.bidi :as bidi]
    [clojure.spec.alpha :as s]
+   [clojure.string :as string]
+   [clojure.tools.cli :refer [parse-opts]]
    [dienstplan.config :refer [config]]
    [dienstplan.endpoints :as endpoints]
    [dienstplan.logging :as logging]
    [dienstplan.middlewares :as middlewares]
    [dienstplan.spec :as spec]
+   [dienstplan.db :as db]
    [mount.core :as mount :refer [defstate]]
    [ring.adapter.jetty :refer [run-jetty]]
    [ring.middleware.cookies :refer [wrap-cookies]]
@@ -15,7 +18,8 @@
    [ring.middleware.keyword-params :refer [wrap-keyword-params]]
    [ring.middleware.params :refer [wrap-params]]
    [ring.middleware.session :refer [wrap-session]]
-   [sentry-clj.core :as sentry]))
+   [sentry-clj.core :as sentry]
+))
 
 (defn wrap-handler
   [handler]
@@ -64,6 +68,56 @@
     (run-jetty app {:port port :join? true}))
   :stop (.stop server))
 
+;; CLI opts
+
+(def run-modes #{:server :migrate :rollback})
+
+(def cli-options
+  [["-m"
+    "--mode MODE"
+    "Run app in the mode specified"
+    :default :server
+    :parse-fn #(keyword (.toLowerCase %))
+    :validate [#(contains? run-modes %) (format "App run modes: %s" run-modes)]]
+   ["-h" "--help" "Print this help message"]])
+
+(defn usage
+  [options-summary]
+  (->> ["dienstplan is a slack bot for duty rotations"
+        ""
+        "Usage: diesntplan [options] action"
+        ""
+        "Options:"
+        options-summary]
+       (string/join \newline)))
+
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (string/join \newline errors)))
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
+
+(defn validate-args
+  [args]
+  (let [{:keys [options _ errors summary]} (parse-opts args cli-options)
+        {:keys [help mode]} options]
+    (cond
+      help {:exit-message (usage summary) :ok? true}
+      errors {:exit-message (error-msg errors)}
+      mode {:mode mode}
+      :else {:exit-message (usage summary) :ok? false})))
+
+;; Entrypoint
+
 (defn -main
   [& args]
-  (mount/start))
+  (let [{:keys [mode exit-message ok?]} (validate-args args)]
+    (if exit-message
+      (exit (if ok? 0 1) exit-message)
+      (case mode
+        :server (mount/start)
+        :migrate (db/migrate)
+        :rollback (db/rollback)))))
+  ;;(mount/start)
