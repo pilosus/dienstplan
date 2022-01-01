207caf1b005a3ebf00859dfc61ac3670b893ce14
Vitaly Samigullin <vrs@pilosus.org>
2022-01-01 22:34:42 +0300

    Improve docs

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject dienstplan "0.1.0-SNAPSHOT"
-  :description "Rotation duty slack bot"
+(defproject dienstplan "0.1.0"
+  :description "Duty rotation slack bot"
   :url "https://github.com/pilosus/dienstplan"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
