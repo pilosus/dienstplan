13d734f99d9d2fd7a52818bbf1ce7a7efb609595
Vitaly Samigullin <vrs@pilosus.org>
2023-06-03 21:02:16 +0200

    Test

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dienstplan "0.3.0"
+(defproject dienstplan "0.3.0-SNAPSHOT"
   :description "Duty rotation slack bot"
   :url "https://github.com/pilosus/dienstplan"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
